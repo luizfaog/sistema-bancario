acf71303961afc26e63c2d372dd47ca0a9c297f4
luizfaog <luiz.felipe008@hotmail.com>
2022-03-17 02:44:13 -0300

    exercício-sistema-bancário

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemaBancário
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conta = new conta();
+            
+            conta.agenciaConta = 001;
+            conta.numeroConta = 4525478315;
+            conta.saldo = 2500;
+
+            var titular = new titular();
+
+            titular.cpfTtitular = 01578525471;
+            titular.nomeTitular = "Luiz Felipe Alves";
+            titular.endereço = "Rua 2, Bairro ABC";
+            
+            Console.WriteLine("O saldo da conta " + conta.numeroConta + " do titular " + titular.nomeTitular + " é: " + conta.Getsaldoatual());
+            
+            conta.GetDepósito(850.88);
+
+            Console.WriteLine("O saldo da conta " + conta.numeroConta + " do titular " + titular.nomeTitular + " após o depósito é: " + conta.Getsaldoatual());
+
+            conta.GetDepósito(1200.50);
+
+            Console.WriteLine("O saldo da conta " + conta.numeroConta + " do titular " + titular.nomeTitular + " após o depósito é: " + conta.Getsaldoatual());
+
+            conta.GetSaque(1800);
+
+            Console.WriteLine("O saldo da conta " + conta.numeroConta + " do titular " + titular.nomeTitular + " após o saque é: " + conta.Getsaldoatual());
+
+            conta.GetSaque(2000);
+
+            Console.WriteLine("O saldo da conta " + conta.numeroConta + " do titular " + titular.nomeTitular + " após o saque é: " + conta.Getsaldoatual());
+
+            conta.GetSaque(2000);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ conta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemaBancário
+{
+    public class conta
+    {
+        public double numeroConta;
+        public double agenciaConta;
+        public double saldo;
+        
+        public double Getsaldoatual()
+        {
+            return saldo;
+        }
+
+        public double GetDepósito(double valor)
+        {
+            saldo = saldo + valor;
+            return saldo;
+        }
+        public double GetSaque(double valor)
+        {
+            if(valor <= saldo)
+            {
+                saldo = saldo - valor;
+            }
+            else
+                Console.WriteLine("O valor de saque é maior do que o valor em conta, por favor forneça um valor menor ou igual ao seu saldo!");
+            return saldo;
+        }
+    }
+    public class titular
+    {
+        public string nomeTitular, endereço;
+        public int cpfTtitular;
+        
+    }
+}
